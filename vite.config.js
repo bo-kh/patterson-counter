9afb00dd21aa7507ae8c687cd341b3acbb6ab66c
Boris Horosh <boris.khorosh@gmail.com>
2022-01-01 16:11:01 +0200

    Added base to config. Fixed a title

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
+  base: '/patterson-counter/',
   build: {
     outDir: './docs',
   },

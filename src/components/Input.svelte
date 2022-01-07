fc211ecb1edf071205a798aa7e2effabf08836c1
Boris Horosh <boris.khorosh@gmail.com>
2022-01-07 21:27:06 +0200

    Fixed an issue: missed minimum width for input element

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -7,3 +7,8 @@
 
 <input bind:value class="grow {bgColor} border-orange-700 border-b-2 focus:outline-none px-2" type="text" disabled={disabled}>
 
+<style>
+  input {
+    min-width: 1rem;
+  }
+</style>
